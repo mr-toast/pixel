c145dfb7aff89a79a5b2b26065bbae9760b57aef
mr-toast <7333784+mr-toast@users.noreply.github.com>
2023-01-02 13:41:51 +0700

    feat(placeholder): add typing effect, sr-text and refactor

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,7 +2,7 @@ import ThemeSwitcher from '@components/themeSwitcher/themeSwitcher'
 
 export default function navbar() {
   return (
-    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div className="container max-w-7xl px-2 sm:px-6 lg:px-8">
       <div className="relative flex h-16 items-center justify-end">
         <ThemeSwitcher />
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,62 @@
 import { type NextPage } from 'next'
 import Head from 'next/head'
-
+import Typist from 'react-typist-component'
 import NavBar from '@components/navbar/navbar'
 
 import Logo from '/public/svg/logo.svg'
 import Mailbox from '/public/svg/mailbox.svg'
+import MailboxFlag from '/public/svg/mailbox-flag.svg'
 
 const Home: NextPage = () => {
+  const Cursor = (
+    <span className="ml-1 inline-block h-4 w-2 animate-pulse bg-black leading-5 dark:bg-mintDark-11"></span>
+  )
+
+  const content = {
+    title: 'Pixel on Pixel',
+    blurb: "HI, I'm a Nextjs and Storybook frontend developer. You can contact me here.",
+    mailLabel: 'message',
+  }
+
   return (
     <>
       <Head>
-        <title>Storybook and Nextjs Frontend Development | Pixel on Pixel</title>
+        <title>Nextjs, Frontend Development and Storybook | Pixel on Pixel</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="description" content="I'm a Storybook and Nextjs frontend developer, get in touch here." />
+        <meta name="description" content="I'm a Nextjs, Storybook developer, get in touch here." />
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
 
-      <NavBar />
-      <div className="grid h-screen grid-rows-[1fr_auto] gap-y-4">
+      <div className="grid h-screen grid-rows-[auto_1fr_auto] gap-y-4">
+        <NavBar />
         <main className="container flex max-w-lg flex-col items-center justify-center px-4">
           <Logo className="mb-8  fill-mint-10 stroke-black opacity-60 dark:fill-transparent dark:stroke-mintDark-11" />
-          <h1 className="sr-only">Pixel on Pixel</h1>
+          <h1 className="sr-only">{content.title}</h1>
 
-          <div className="my-12 flex items-center">
-            <p className=" text-xl lowercase leading-none sm:text-3xl">&gt; Hand crafted binary</p>
-            <div className="ml-4 h-5 w-2 animate-pulse bg-black dark:bg-mintDark-11 sm:h-7 sm:w-3" />
+          <div className="mx-auto mb-4 flex h-16 w-full gap-2 text-left sm:w-4/5">
+            <span className="grow-0">&gt;</span>
+            <div className="relative grow">
+              <p className="absolute top-0 left-0" aria-hidden>
+                <Typist startDelay={1000} typingDelay={70} cursor={Cursor} loop finishDelay={5000}>
+                  {content.blurb}
+                </Typist>
+              </p>
+              <span className="sr-only">{content.blurb}</span>
+            </div>
           </div>
 
           <a href="mailto:hello@pixelonpixel.com">
-            <Mailbox className="icon mx-auto fill-black dark:fill-mintDark-11" />
-            <p className="">say hi!</p>
+            <div className="group relative mx-auto mb-2 h-16 w-16">
+              <Mailbox className="icon absolute stroke-black dark:stroke-mintDark-11" />
+              <MailboxFlag className="icon absolute fill-transparent stroke-black  transition  group-hover:-scale-x-100 group-hover:fill-mint-8 dark:stroke-mintDark-11 dark:stroke-mintDark-11  dark:group-hover:fill-mintDark-8" />
+            </div>
+            <p className="text-center text-xs">{content.mailLabel}</p>
           </a>
         </main>
 
         <footer className="container p-4 text-center text-xs">
-          © Copyright {new Date().getFullYear()} | Pixel on Pixel
+          © Copyright {new Date().getFullYear()} | {content.title}
         </footer>
       </div>
     </>
