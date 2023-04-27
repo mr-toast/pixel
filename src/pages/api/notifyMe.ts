c34ec0f9e7b892f944d2140adf19f4bde7b92f55
mr-toast <7333784+mr-toast@users.noreply.github.com>
2023-04-27 22:15:06 +0700

    feat(api): add email endpoint

---
+++ src/pages/api/notifyMe.ts
@@ -0,0 +1,43 @@
+const nodemailer = require('nodemailer')
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type responseData = {
+  message: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<responseData>) {
+  const data = req.body
+
+  const sender = {
+    name: process.env.EMAIL_NAME,
+    address: process.env.EMAIL_SENDER,
+  }
+
+  const emailBody = `<body><p>${data.name} wrote,</p><p>${data.message}</p></body>`
+
+  let transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_SERVER_USER,
+      pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+  })
+
+  try {
+    let info = await transporter.sendMail({
+      from: sender,
+      to: [process.env.EMAIL_RECIPIENT],
+      subject: `Site Message: ${data.subject}`,
+      text: `${data.name} wrote, - ${data.message}`,
+      html: emailBody,
+    })
+
+    console.log('Message sent: %s', info.messageId)
+    res.status(200).json({ message: 'Email sent' })
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ message: 'Error adding document' })
+  }
+}
