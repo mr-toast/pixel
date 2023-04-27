d39ea2beda7b55694eb6b6cb17b81567cc1f5fda
mr-toast <7333784+mr-toast@users.noreply.github.com>
2023-04-27 23:01:40 +0700

    feat(page): remove background image and colors

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,24 @@
-import { Image } from '~/components/image/'
+// import { Image } from '~/components/image/'
 import { Link } from '~/components/link'
 export default function Custom404() {
   return (
     <>
       <main className="relative isolate h-screen">
-        <Image
+        {/* <Image
           src="/images/video-hero-poster.png"
           alt=""
           width={1440}
           height={850}
           className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
-        />
+        /> */}
         <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-          <p className="text-base font-semibold leading-8 text-zinc-50">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-zinc-50 sm:text-5xl">Page not found</h1>
-          <p className="mt-4 text-base text-zinc-50/70 sm:mt-6">
+          <p className="text-base font-semibold leading-8 text-zinc-950">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-zinc-950 sm:text-5xl">Page not found</h1>
+          <p className="mt-4 text-base text-zinc-950/70 sm:mt-6">
             Sorry we&apos;ve looked everywhere, I mean everywhere, and we can&apos;t find it.
           </p>
           <div className="mt-10 flex justify-center">
-            <Link href="/" className="text-sm font-semibold leading-7 text-zinc-50">
+            <Link href="/" className="text-sm font-semibold leading-7 text-zinc-950">
               <span aria-hidden="true">&larr;</span> Back to home
             </Link>
           </div>
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,27 +1,27 @@
-import { Image } from '~/components/image'
+// import { Image } from '~/components/image'
 import { Link } from '~/components/link'
 
 export default function Custom500() {
   return (
     <>
       <main className="relative isolate h-screen">
-        <Image
+        {/* <Image
           src="/images/video-hero-poster.png"
           alt=""
           width={1440}
           height={850}
-          className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
-        />
+          className="absolute inset-0 -z-10 h-full w-full object-cover object-top opacity-10"
+        /> */}
         <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-          <p className="text-base font-semibold leading-8 text-zinc-50">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-zinc-50 sm:text-5xl">
+          <p className="text-base font-semibold leading-8 text-zinc-950">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-zinc-950 sm:text-5xl">
             500 - Server-side error occurred
           </h1>
-          <p className="mt-4 text-base text-zinc-50/70 sm:mt-6">
+          <p className="mt-4 text-base text-zinc-950/70 sm:mt-6">
             It&apos;s bad, I mean really bad, there&apos;s fire and tears and stuff.
           </p>
           <div className="mt-10 flex justify-center">
-            <Link href="/" className="text-sm font-semibold leading-7 text-zinc-50">
+            <Link href="/" className="text-sm font-semibold leading-7 text-zinc-950">
               <span aria-hidden="true">&larr;</span> Start over
             </Link>
           </div>
