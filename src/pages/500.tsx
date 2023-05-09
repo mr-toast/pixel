3c532a43f5ed7d89be0404869e0dd03f4717114b
mr-toast <7333784+mr-toast@users.noreply.github.com>
2023-05-09 19:32:02 +0700

    feat(page): clean up and simplify error pages

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,33 +1,16 @@
-// QUESTION TODO can 500 pages be embelished? If so bring it inline with the 404 page
-
-// import { Image } from '~/components/image'
 import { Link } from '~/components/link'
 
 export default function Custom500() {
   return (
     <>
-      <main className="relative isolate h-screen">
-        {/* <Image
-          src="/images/video-hero-poster.png"
-          alt=""
-          width={1440}
-          height={850}
-          className="absolute inset-0 -z-10 h-full w-full object-cover object-top opacity-10"
-        /> */}
-        <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-          <p className="text-base font-semibold leading-8 text-zinc-950">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-zinc-950 sm:text-5xl">
-            500 - Server-side error occurred
-          </h1>
-          <p className="mt-4 text-base text-zinc-950/70 sm:mt-6">
-            It&apos;s bad, I mean really bad, there&apos;s fire and tears and stuff.
-          </p>
-          <div className="mt-10 flex justify-center">
-            <Link href="/" className="text-sm font-semibold leading-7 text-zinc-950">
-              <span aria-hidden="true">&larr;</span> Start over
-            </Link>
-          </div>
-        </div>
+      <main className="bg-zinc-950">
+        <div className="flex flex-col justify-center items-center h-screen mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+          <h1 className="mb-2 text-2xl font-bold tracking-tight text-zinc-50 sm:text-5xl">500</h1>
+          <p className="mb-8 text-zinc-50">It&apos;s bad, I mean really bad, there&apos;s fire and tears and stuff.</p>
+					<Link href="/" format="button" color="white"className="hover:bg-zinc-50 max-w-min px-6 py-1 rounded-full animate-pulse hover:animate-none">
+						<span className="">reset</span>
+					</Link>
+				</div>
       </main>
     </>
   )
