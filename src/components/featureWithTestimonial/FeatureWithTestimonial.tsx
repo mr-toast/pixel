917a1b34f05b16ca0e0a2372b1e07fef04068113
mr-toast <7333784+mr-toast@users.noreply.github.com>
2023-05-09 19:35:16 +0700

    feat(component): add modal and gallery funtionality

--- src/components/featureWithTestimonial/FeatureWithTestimonial.tsx
+++ src/components/featureWithTestimonial/FeatureWithTestimonial.tsx
@@ -1,6 +1,14 @@
+import { useResponsive } from 'ahooks'
+import { Fragment, useState } from 'react'
+import { Dialog , Transition} from '@headlessui/react'
+import { useTheme } from 'next-themes'
 import { Image } from '~/components/image'
+import { Button } from '~/components/button'
 import { Link } from '~/components/link'
 import { Typist } from '~/components/typist'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid'
+import GithubIcon from '/public/svg/icon/github.svg'
 import AlgoliaIcon from '/public/svg/stack/algolia.svg'
 import CmsIcon from '/public/svg/stack/cms.svg'
 import DockerIcon from '/public/svg/stack/docker.svg'
@@ -37,8 +45,29 @@ const stackIconsMap: StackIconsMapType = {
   wordpress: WordpressIcon,
 }
 
+
+const constants = {
+  OPEN_TEXT: 'Open Gallery',
+  CLOSE_TEXT: 'Close Gallery',
+}
+
 export function FeatureWithTestimonial({ featuredWork }: FeatureWithTestimonialProps) {
-  const { header, title, description, stack, button, testimonial, imageUrl } = featuredWork
+	let [isOpen, setIsOpen] = useState(false)
+
+	const responsive = useResponsive()
+	const { theme } = useTheme()
+	
+  const featuredWorkGallery = responsive?.md ? featuredWork.galleryDesktop : featuredWork.gallery
+	
+	function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+
 
   return (
     <div className="overflow-hidden py-24 sm:py-32">
@@ -47,46 +76,120 @@ export function FeatureWithTestimonial({ featuredWork }: FeatureWithTestimonialP
           <div className="lg:pr-4 lg:pt-4">
             <div className="lg:max-w-lg">
               <h2 className="text-base font-semibold leading-7">
-                <Typist withPrompt>{header}</Typist>
+                <Typist withPrompt>{featuredWork.header}</Typist>
               </h2>
 
-              <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">{title}</p>
-              <p className="mt-6 text-lg leading-8 text-zinc-700 dark:text-zinc-300">{description}</p>
+              <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">{featuredWork.title}</p>
+              <p className="mt-6 text-lg leading-8 text-zinc-700 dark:text-zinc-300">{featuredWork.description}</p>
 
               <div className="mt-8 flex flex-wrap gap-4">
-                {stack?.map((item: StackIcons) => {
+                {featuredWork.stack?.map((item: StackIcons) => {
                   const Icon = stackIconsMap[item]
                   return <Icon key={item} className="h-12 w-12" aria-hidden="true" />
                 })}
               </div>
 
               <div className="mt-8">
-                <Link format="button" color="black" href={button.href}>
-                  {button.label}
-                </Link>
+                <Button color="black" onClick={openModal}>
+                  {featuredWork.button.label}
+                </Button>
+
+
+								<Transition appear show={isOpen} as={Fragment}>
+									{/* HACK using this className __className_f820ce becuase I don't know how to get the custom font to apply */}
+									<Dialog as="div" className="relative z-50 __className_f820ce" onClose={closeModal}>
+										<Transition.Child
+											as={Fragment}
+											enter="ease-out duration-300"
+											enterFrom="opacity-0"
+											enterTo="opacity-100"
+											leave="ease-in duration-200"
+											leaveFrom="opacity-100"
+											leaveTo="opacity-0"
+										>
+											<div className="fixed inset-0 bg-zinc-950 bg-opacity-25" />
+										</Transition.Child>
+
+										<div className="fixed inset-0 overflow-y-auto bg-zinc-50 dark:bg-zinc-950">
+											<div className="flex min-h-full items-center justify-center text-center">
+												<Transition.Child
+													as={Fragment}
+													enter="ease-out duration-300"
+													enterFrom="opacity-0 scale-95"
+													enterTo="opacity-100 scale-100"
+													leave="ease-in duration-200"
+													leaveFrom="opacity-100 scale-100"
+													leaveTo="opacity-0 scale-95"
+												>
+													<Dialog.Panel className="w-full transform overflow-hidden bg-zinc-50 dark:bg-zinc-950 p-6 text-left align-middle transition-all">
+														<div className="flex items-center justify-center">
+															<Button
+																color={theme === 'dark' ? 'white' : 'black' }
+																format="icon"
+																className=""		
+																onClick={closeModal}
+															>
+																<span className="sr-only">{constants.CLOSE_TEXT}</span>
+																<XMarkIcon className="h-6 w-6" aria-hidden="true" />
+															</Button>
+														</div>
+														<div className="mt-8 flow-root text-center">
+															<div className="mb-8 rounded-md bg-zinc-400/10 dark:bg-zinc50/5 p-6">
+																<div className="max-w-prose mx-auto">
+																	<h4 className="mb-4 text-2xl">{featuredWork.title}</h4>
+																	<p>{featuredWork.description}</p>
+																	<div className="flex gap-4 justify-center mt-6">
+																		{featuredWork.gitUrl && (
+																		<Link href={featuredWork.gitUrl}>
+																			<GithubIcon className="h-6 w-6" />
+																		</Link>
+																		)}
+																		{featuredWork.siteUrl && (
+																		<Link href={featuredWork.siteUrl}>
+																			<ArrowTopRightOnSquareIcon className="h-6 w-6" />
+																		</Link>
+																		)}	
+																	</div>
+																</div>
+															</div>
+															<div className="flex flex-col gap-6">
+																{featuredWorkGallery && (featuredWorkGallery.map((item, index) => (
+																	<Image className="rounded-xl shadow-xl" src={item} alt="" width={640} height={376} key={index} />
+																)))}
+															</div>
+														</div>
+													
+													</Dialog.Panel>
+												</Transition.Child>
+											</div>
+										</div>
+									</Dialog>
+								</Transition>
+
               </div>
-              <figure className="mt-16 border-l border-zinc-700 pl-8 text-zinc-700 dark:border-zinc-100 dark:text-zinc-300">
+              {featuredWork.testimonial && (
+							<figure className="mt-16 border-l border-zinc-700 pl-8 text-zinc-700 dark:border-zinc-100 dark:text-zinc-300">
                 <blockquote className="text-base leading-7">
-                  <p>&ldquo;{testimonial.quote}&rdquo;</p>
+                  <p>&ldquo;{featuredWork.testimonial.quote}&rdquo;</p>
                 </blockquote>
                 <figcaption className="mt-6 flex gap-x-4 text-sm leading-6">
                   <Image
-                    src={testimonial.avatarUrl}
+                    src={featuredWork.testimonial.avatarUrl}
                     alt=""
                     className="h-6 w-6 flex-none rounded-full"
                     width={48}
                     height={48}
                   />
                   <div>
-                    <span className="font-semibold text-zinc-950 dark:text-zinc-50">{testimonial.name}</span>{' '}
-                    {testimonial.title}
+                    <span className="font-semibold text-zinc-950 dark:text-zinc-50">{featuredWork.testimonial.name}</span>{' '}
+                    {featuredWork.testimonial.title}
                   </div>
                 </figcaption>
-              </figure>
+              </figure>)}
             </div>
           </div>
           <Image
-            src={imageUrl}
+            src={featuredWork.imageUrl}
             alt=""
             className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-zinc-400/10 sm:w-[57rem] md:-ml-4 lg:ml-0"
             width={2432}
